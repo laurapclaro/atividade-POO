79eb5921abff2ef1133e454f918014c4973c6238
laura <laurapapa693@gmail.com>
2026-03-02 11:29:42 -0300

    criando classe item_biblioteca

---
+++ item_biblioteca.py
@@ -0,0 +1,18 @@
+class Item_biblioteca:
+    def __init__(self, codigo, titulo, ano, disponivel):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__disponivel = disponivel
+
+    
+def exibir_detalhes(self):
+    if self.__disponivel:
+
+
+def emprestar(self):
+
+    
+def devolver(self):
+    
+    
